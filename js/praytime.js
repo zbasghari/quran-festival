801e6a826fadb1ae587a90e729788278589afa4a
zizi <97338702+zbasghari@users.noreply.github.com>
2026-05-11 04:47:55 -0700

    Create praytime.js

---
+++ js/praytime.js
@@ -0,0 +1,51 @@
+// Prayer Times Data (Sample data for Tehran)
+const prayerTimesData = {
+    tehran: {
+        fajr: "05:15",
+        sunrise: "06:45",
+        dhuhr: "12:30",
+        sunset: "18:15",
+        maghrib: "18:35",
+        midnight: "00:00"
+    },
+    mashhad: {
+        fajr: "05:00",
+        sunrise: "06:30",
+        dhuhr: "12:15",
+        sunset: "18:00",
+        maghrib: "18:20",
+        midnight: "23:45"
+    },
+    isfahan: {
+        fajr: "05:20",
+        sunrise: "06:50",
+        dhuhr: "12:35",
+        sunset: "18:20",
+        maghrib: "18:40",
+        midnight: "00:05"
+    },
+    shiraz: {
+        fajr: "05:25",
+        sunrise: "06:55",
+        dhuhr: "12:40",
+        sunset: "18:25",
+        maghrib: "18:45",
+        midnight: "00:10"
+    },
+    tabriz: {
+        fajr: "05:10",
+        sunrise: "06:40",
+        dhuhr: "12:25",
+        sunset: "18:10",
+        maghrib: "18:30",
+        midnight: "23:55"
+    },
+    qom: {
+        fajr: "05:18",
+        sunrise: "06:48",
+        dhuhr: "12:32",
+        sunset: "18:17",
+        maghrib: "18:37",
+        midnight: "00:02"
+    }
+};
